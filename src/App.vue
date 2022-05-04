6599705814df2c025d5067c3f01f337af896c936
unknown <yuag1331@gmail.com>
2022-05-04 23:29:09 +0800

    meta\

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,43 @@ import { useMeta } from 'vue-meta'
 
 export default {
   setup () {
-    useMeta({
+    const { meta } = useMeta({
       title: '1111',
+      base: { href: '/vue-router', target: '_blank' },
+      charset: 'utf8',
+      og: {
+        title: 'Og Title',
+        description: 'Bla bla',
+        image: [
+          'https://picsum.photos/600/400/?image=80',
+          'https://picsum.photos/600/400/?image=82'
+        ]
+      },
+      twitter: {
+        title: 'Twitter Title'
+      },
+      noscript: [
+        { tag: 'link', rel: 'stylesheet', href: 'style.css' }
+      ],
+      otherNoscript: {
+        tag: 'noscript',
+        'data-test': 'hello',
+        children: [
+          { tag: 'link', rel: 'stylesheet', href: 'style2.css' }
+        ]
+      },
+      body: 'body-script1.js', // TODO: fix
+      bodyAttrs: {
+        class: ['theme-dark']
+      },
+      script: [
+        { src: 'head-script1.js' },
+        { src: 'body-script2.js', to: 'body' },
+        { src: 'body-script3.js', to: '#body-prepend' }
+      ],
       htmlAttrs: { 
-          lang: 'constants.meta.language',
+          amp: true,
+          lang: ['en']
         },
         description: 'constants.main.description',
         meta: [
@@ -31,6 +64,8 @@ export default {
             }
           ]
     })
+    // 兩秒 title 更換
+    setTimeout(() => (meta.title = 'My Updated Title'), 2000)
   }
 }
 </script>

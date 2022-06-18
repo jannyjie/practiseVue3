139a3706d9a7f64d3179432211efd6662d8f9333
unknown <yuag1331@gmail.com>
2022-06-18 12:30:24 +0800

    head

--- src/components/meta.vue
+++ src/components/meta.vue
@@ -70,57 +70,57 @@ export default {
         "快速驗證1111會員身分，取得專屬會員卡，即可參與會員專屬活動，享受尊榮禮遇身分",
       keywords: "keywords",
       meta: [
-        {
-          property: "og:type",
-          content: "website",
-        },
-        {
-          property: "og:locale",
-          content: "zh_TW",
-        },
-        {
-          property: "og:title",
-          content: "驗證中心|1111人力銀行",
-        },
-        {
-          property: "og:url",
-          content: "https://jannyjie.github.io/practiseVue3/dist/index.html",
-        },
+        // {
+        //   property: "og:type",
+        //   content: "website",
+        // },
+        // {
+        //   property: "og:locale",
+        //   content: "zh_TW",
+        // },
+        // {
+        //   property: "og:title",
+        //   content: "驗證中心|1111人力銀行",
+        // },
+        // {
+        //   property: "og:url",
+        //   content: "https://jannyjie.github.io/practiseVue3/dist/index.html",
+        // },
         {
           property: "og:image",
           content:
-            "https://event.1111.com.tw/event22/2022membercard/assets/img/og.jpg",
+            "https://event.1111.com.tw/event22/admission_reserve/assets/img/backgrounds/1.jpg",
         },
-        {
-          property: "og:description",
-          content:
-            "快速驗證1111會員身分，取得專屬會員卡，即可參與會員專屬活動，享受尊榮禮遇身分",
-        },
-        {
-          name: "twitter:title",
-          content: "驗證中心|1111人力銀行",
-        },
-        {
-          name: "twitter:description",
-          content:
-            "快速驗證1111會員身分，取得專屬會員卡，即可參與會員專屬活動，享受尊榮禮遇身分",
-        },
-        {
-          name: "twitter:keywords",
-          content: "驗證、快速通關、會員卡",
-        },
-        {
-          name: "google",
-          content: "notranslate",
-        },
-        {
-          name: "robots",
-          content: "max-image-preview:large, max-video-preview:-1",
-        },
-        {
-          "http-equiv": "Content-Language",
-          content: "zh-Hant",
-        },
+        // {
+        //   property: "og:description",
+        //   content:
+        //     "快速驗證1111會員身分，取得專屬會員卡，即可參與會員專屬活動，享受尊榮禮遇身分",
+        // },
+        // {
+        //   name: "twitter:title",
+        //   content: "驗證中心|1111人力銀行",
+        // },
+        // {
+        //   name: "twitter:description",
+        //   content:
+        //     "快速驗證1111會員身分，取得專屬會員卡，即可參與會員專屬活動，享受尊榮禮遇身分",
+        // },
+        // {
+        //   name: "twitter:keywords",
+        //   content: "驗證、快速通關、會員卡",
+        // },
+        // {
+        //   name: "google",
+        //   content: "notranslate",
+        // },
+        // {
+        //   name: "robots",
+        //   content: "max-image-preview:large, max-video-preview:-1",
+        // },
+        // {
+        //   "http-equiv": "Content-Language",
+        //   content: "zh-Hant",
+        // },
       ],
       htmlAttrs: {
         lang: "zh-Hant-TW",
